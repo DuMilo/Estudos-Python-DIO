b319a4d9439464556330731aa15f882bb5149337
DuMilo <milomoreirac@gmail.com>
2024-09-18 21:19:07 -0300

    correcao e adicao de ponto e virgula

--- Lista_de_Exercicios_Repeticao/atv_estrutura_repeticao_for.py
+++ Lista_de_Exercicios_Repeticao/atv_estrutura_repeticao_for.py
@@ -1,13 +1,13 @@
-VOGAIS = "AEIOU"
+VOGAIS = "AEIOU";
 
 while True:
-    texto = input("Informe um texto (ou 'pare' para sair): ")
+    texto = input("Informe um texto (ou 'pare' para sair): ");
     
     if texto.lower() == 'pare' or 'stop':
-        break
+        break;
 
     for letra in texto:
         if letra.upper() in VOGAIS:
-            print(letra, end=" ")
+            print(letra, end=" ");
     
-    print()  # Para pular para a próxima linha após imprimir as vogais
+    print();
