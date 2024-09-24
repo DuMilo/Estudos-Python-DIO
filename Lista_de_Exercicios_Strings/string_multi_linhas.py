544c8c0aa0b18fc13d33ed9669f5dcc0eab4ac14
Milo Moreira <milomoreirac@gmail.com>
2024-09-24 15:52:02 -0300

    added multi linhas file

---
+++ Lista_de_Exercicios_Strings/string_multi_linhas.py
@@ -0,0 +1,13 @@
+nome = "Milo";
+
+mensagem = f"""
+Olá meu nome é {nome},
+Eu estou aprendendo Python.
+""";
+
+print(mensagem);
+
+# Saída:
+
+# >> Olá meu nome é Milo,
+# >> Eu estou aprendendo Python.
