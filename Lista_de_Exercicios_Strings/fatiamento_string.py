008a7474fed00fdcaed2242dcbdcd004bd20abe9
Milo Moreira <milomoreirac@gmail.com>
2024-09-24 15:42:56 -0300

    fatiamento string file

---
+++ Lista_de_Exercicios_Strings/fatiamento_string.py
@@ -0,0 +1,22 @@
+nome = "Milo Moreira e Castro";
+
+print(nome[0]);
+# >> M
+
+print(nome[:4]);
+# >> Milo
+
+print(nome[5:]);
+# >> Moreira e Castro
+
+print(nome[5:12]);
+# >> Moreira
+
+print(nome[5:12:2]);
+# >> Mria
+
+print(nome[:]);
+# >> Milo Moreira e Castro
+
+print(nome[::-1]);
+# >> ortsaC e arieroM oliM
