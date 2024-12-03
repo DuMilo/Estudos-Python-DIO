279dd6d04fdb1bb64e3823b97b7f04b343ea183d
Milo Moreira <milomoreirac@gmail.com>
2024-12-02 21:04:43 -0300

    fixing poo challenge

--- 03 - Programação Orientada a Objetos com Python/01 - Classes e Objetos/01_criando_classe_bicicleta.py
+++ 03 - Programação Orientada a Objetos com Python/01 - Classes e Objetos/01_criando_classe_bicicleta.py
@@ -14,8 +14,12 @@ class Bicicleta:
     def correr(self):
         print("Vrum vrum!");
 
+    def __str__(self):
+        return f"{self.__class__.__name__}: {", ".join([f"{chave}={valor}" for chave, valor in self.__dict__.items()])}"
+
 bicicleta_1 = Bicicleta("Amarela", "Grande", "2007", "200");
 
 bicicleta_1.buzinar()
 bicicleta_1.parar()
 bicicleta_1.correr()
+print(bicicleta_1.__str__())
--- 03 - Programação Orientada a Objetos com Python/README.md
+++ 03 - Programação Orientada a Objetos com Python/README.md
@@ -1,3 +1,3 @@
 # Atenção!
 
-#### Esse conteúdo de POO vai ser muito extenso, portanto separei as etapas em várias pastas para melhor entendimento do conteúdo.
+### Esse conteúdo de POO vai ser muito extenso, portanto separei as etapas em várias pastas para melhor entendimento do conteúdo.
